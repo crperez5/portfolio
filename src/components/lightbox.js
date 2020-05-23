3703fd4e4c73e4a07f1ae8ed40a16e2d997b3b1d
Cristian Perez <c.perez@detego.com>
2020-05-23 20:50:14 +0300

    Working on the proyects page

--- src/components/lightbox.js
+++ src/components/lightbox.js
@@ -3,8 +3,11 @@ import styled from "styled-components"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
 
+const Figure = styled.figure`
+  cursor: pointer;
+`
 const LightboxModal = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
@@ -15,17 +18,21 @@ const LightboxModal = styled.div`
   background: rgba(0, 0, 0, 0.5);
   opacity: ${props => (props.visible ? "1" : "0")};
   visibility: ${props => (props.visible ? "visible" : "hidden")};
+  transition: opacity 0.25s ease-in;
+  z-index: 9999;
 `
 const LightboxModalContent = styled.div`
   margin: 15px;
   max-width: ${props => props.maxWidth};
   width: 100%;
 `
-
+const LightboxModalContentCloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 1rem;
+`
 const Lightbox = props => {
-  const aspectRatio = props.aspectRatio
-
-  const fluid = { ...props.fluid, aspectRatio }
 
   const [showLightbox, setShowLightbox] = useState(false)
 
@@ -36,12 +43,15 @@ const Lightbox = props => {
 
   return (
     <>
-      <figure onClick={handleClick} class="image is-16by9">
-        <Img fluid={fluid} />
-      </figure>
-      <LightboxModal visible={showLightbox}>
+      <Figure onClick={handleClick}>
+        <Img fluid={{ ...props.fluid, aspectRatio: props.aspectRatio }} />
+      </Figure>
+      <LightboxModal onClick={handleClick} visible={showLightbox}>
         <LightboxModalContent maxWidth={props.maxWidth}>
           <Img fluid={props.fluid} />
+          <LightboxModalContentCloseButton className="delete is-large">
+            {" "}
+          </LightboxModalContentCloseButton>
         </LightboxModalContent>
       </LightboxModal>
     </>
