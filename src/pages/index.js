78098b680368b48777ea0d9cd5c1ab76336c9f9d
Cristian Perez <c.perez@detego.com>
2020-06-08 18:24:47 +0300

    Implementing a responsive design, front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,11 @@ import SEO from "../components/seo"
 import Img from "gatsby-image"
 import { useTranslation } from "react-i18next"
 import styled from "styled-components"
+import theme from "../theme"
 
 export const query = graphql`
   query {
-    image: file(relativePath: { eq: "me.png" }) {
+    imageLarge: file(relativePath: { eq: "me.png" }) {
       childImageSharp {
         fixed(
           duotone: { highlight: "#ec8b5e", shadow: "#141a46" }
@@ -20,6 +21,40 @@ export const query = graphql`
         }
       }
     }
+    imageMedium: file(relativePath: { eq: "me.png" }) {
+      childImageSharp {
+        fixed(
+          duotone: { highlight: "#ec8b5e", shadow: "#141a46" }
+          width: 400
+          height: 400
+          quality: 100
+        ) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    imageSmall: file(relativePath: { eq: "me.png" }) {
+      childImageSharp {
+        fixed(
+          duotone: { highlight: "#ec8b5e", shadow: "#141a46" }
+          width: 275
+          height: 275
+          quality: 100
+        ) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    image: file(relativePath: { eq: "me.png" }) {
+      childImageSharp {
+        fluid(
+          duotone: { highlight: "#ec8b5e", shadow: "#141a46" }
+          maxWidth: 550
+        ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
 `
 const IndexPage = ({ data }) => {
@@ -28,55 +63,123 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO />
-      <section class="section">
-        <div class="section">
-          <div class="container">
-            <div class="columns is-centered">
-              <div class="column">
-                <div class="column is-offset-1">
-                  <Title>Cristian Pérez</Title>
-                  <div class="column">
-                    <div class="content">
-                      <div class="column is-four-fifths is-marginless is-paddingless">
-                        <h3 class="has-text-weight-light">
-                          Front-end engineer specializing in React with a focus
-                          on performance. Love huskies, really afraid of bees.
-                          
-                        </h3>
-                        
-                        <h4>#react #apollo #redux #webperf</h4>
-                        <br/>
-                        
-                        <div class="button is-primary is-large is-rounded">
-                          Want to know more? Continue
-                        </div>
+      <div class="section">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column">
+              <div class="column is-offset-1 is-offset-1-widescreen is-offset-1-desktop is-offset-1-tablet">
+                <Title theme={theme}>Cristian Pérez</Title>
+                <div class="column">
+                  <div class="content">
+                    <div class="column is-full-widescreen is-full-desktop is-full-size-tablet is-marginless is-paddingless">
+                      <h3 class="has-text-weight-light is-size-4-fullhd is-size-4-widescreen is-size-4-desktop is-size-5-tablet is-size-5-mobile">
+                        Front-end engineer specializing in React with a focus on
+                        performance. Love huskies, really afraid of bees.
+                      </h3>
+
+                      <h4 class="is-size-5-fullhd is-size-5-widescreen is-size-5-desktop is-size-6-tablet is-size-6-mobile">
+                        #react #apollo #redux #webperf
+                      </h4>
+                      <br />
+
+                      <div class="button is-fullwidth-mobile is-primary is-size-4-fullhd is-size-5-widescreen is-size-5-desktop is-rounded">
+                        Want to know more? Continue
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="column is-background">
-                <ImgContainer translateX="-65px">
-                  <Img
-                    className="has-radius-275"
-                    fixed={data.image.childImageSharp.fixed}
-                  />
-                </ImgContainer>
-              </div>
+            </div>
+            <div class="column is-background">
+              <StyledImg
+                theme={theme}
+                className="has-radius-275"
+                fluid={data.image.childImageSharp.fluid}
+              />
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </Layout>
   )
 }
 
 export default IndexPage
 
-const Title = styled.h1`
-  font: 9rem Monoton, sans-serif;
-  line-height: 10rem;
+const StyledImg = styled(Img)`
+${({ theme }) => {
+  return theme.mixins.smallScreen(`
+    transform: translateX(-10px);
+    overflow: hidden;
+    width: 300px;
+    height: 300px;
+    bottom: -79px;
+    left: -82px;
+    z-index: -1;
+  `)
+}}
+
+  ${({ theme }) => {
+    return theme.mixins.mediumScreen(`
+      width: 275px;
+      height: 275px;
+      transform: translateX(-30px);
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.mediumLargeScreen(`
+    width: 400px;
+    height: 400px;
+    transform: translateX(-40px);
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.largeScreen(`
+      width: 400px;
+      height: 400px;
+      transform: translateX(-40px);
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.extraLargeScreen(`
+      width: 550px;
+      height: 550px;
+      transform: translateX(-65px);
+    `)
+  }}  
 `
-const ImgContainer = styled.div`
-  transform: translateX(${props => props.translateX});
+
+const Title = styled.h1`
+  font: 9rem Monoton;
+  line-height: 10rem;
+  ${({ theme }) => {
+    return theme.mixins.smallScreen(`
+    font-size: 4rem;
+    line-height: 4.5rem;
+    
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.mediumScreen(`
+      font-size: 6.5rem;
+      line-height: 7rem;
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.mediumLargeScreen(`
+    font-size:8rem;
+    line-height: 9rem;
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.largeScreen(`
+      font-size:8rem;
+      line-height: 9rem;
+    `)
+  }}
+  ${({ theme }) => {
+    return theme.mixins.extraLargeScreen(`
+      font-size:9rem;
+    `)
+  }}
 `
--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,37 @@
+import variables from "./_variables.scss"
+
 export default {
   mixins: {
     from: (device, content) => `
-    @media screen and (min-width: ${device}) {
+    @media screen and (min-width: ${device}px) {
         ${content}
     }`,
+    smallScreen: content => `
+    @media screen and (min-width: 0) and (max-width: ${variables.tablet}) {
+      ${content}
+  }
+    `,
+    mediumScreen: content => `
+    @media screen and (min-width: ${variables.tablet}) and (max-width: ${
+      variables.desktop}) {
+      ${content}
+  }
+    `,
+    mediumLargeScreen: content => `
+    @media screen and (min-width: ${variables.desktop}) and (max-width: ${
+      variables.widescreen}) {
+      ${content}
+  }
+    `,
+    largeScreen: content => `
+    @media screen and (min-width: ${variables.widescreen}) and (max-width: ${variables.fullhd}) {
+      ${content}
+  }
+    `,
+    extraLargeScreen: content => `
+    @media screen and (min-width: ${variables.fullhd}) {
+      ${content}
+  }
+    `,
   },
 }
