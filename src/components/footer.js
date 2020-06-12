27f08cd85cfd3a43138d852a464fe89d31ebf9eb
Cristian Perez <c.perez@detego.com>
2020-06-12 16:39:19 +0300

    Translating footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,43 @@
 import React from "react"
-export default () => (
-  <footer class="footer">
-    <div class="container">
-      <div class="level">
-        <div class="level-left">
-          <div class="level-item">
-            © {new Date().getFullYear()}, Built with&nbsp;
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+import { useTranslation } from "react-i18next"
+import { useStaticQuery, graphql } from "gatsby"
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          authorName
+        }
+      }
+    }
+  `)
+
+  const { t } = useTranslation()
+  return (
+    <footer class="footer">
+      <div class="container">
+        <div class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <span class="is-hidden-mobile" style={{ "margin-right": "5px" }}>
+                {data.site.siteMetadata.authorName}
+              </span>
+              © {new Date().getFullYear()}, {t("footer.builtWith")}&nbsp;
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </div>
           </div>
-        </div>
-        <div class="level-right">
-          <div class="level-item">
-            <a href="http://localhost:8000/en">In English</a>
-          </div>
-          <div class="level-item is-hidden-mobile">|</div>
-          <div class="level-item">
-            <a href="http://localhost:8000/es">En Español</a>
+          <div class="level-right">
+            <div class="level-item">
+              <a href="http://localhost:8000/en">{t("footer.english")}</a>
+            </div>
+            <div class="level-item is-hidden-mobile">|</div>
+            <div class="level-item">
+              <a href="http://localhost:8000/es">{t("footer.spanish")}</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
