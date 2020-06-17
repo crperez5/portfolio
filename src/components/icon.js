a321e6e5a53c53e182cdecfc999b4ab42714a344
Cristian Perez <c.perez@detego.com>
2020-06-17 21:56:29 +0300

    disabling icons test

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,17 +1,17 @@
-import React from "react"
+// import React from "react"
 
-const reqSvgs = require.context("!svg-react-loader?!../images/icons", true, /\.svg$/)
+// const reqSvgs = require.context("!svg-react-loader?!../images/icons", true, /\.svg$/)
 
-const svgMap = reqSvgs.keys().reduce((images, path) => {
-  const key = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf("."))
-  images[key] = reqSvgs(path).default
-  return images
-}, {})
+// const svgMap = reqSvgs.keys().reduce((images, path) => {
+//   const key = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf("."))
+//   images[key] = reqSvgs(path).default
+//   return images
+// }, {})
 
-const Icon = ({ icon, fill, width, height }) => {
-  const Svg = svgMap[icon]
+// const Icon = ({ icon, fill, width, height }) => {
+//   const Svg = svgMap[icon]
 
-  return <Svg fill={fill} width={width} height={height} />
-}
+//   return <Svg fill={fill} width={width} height={height} />
+// }
 
-export default Icon
+// export default Icon
